318bf72c924432a4cf00ac8117ee1da77b78ac46
Verviam <gaoevan37@gmail.com>
2023-10-22 19:56:09 -0400

    practicei

--- index.js
+++ index.js
@@ -93,25 +93,55 @@ console.log(bookInfo);
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 
-const sortedDates = 
-  books.forEach(publishedDate => {
-  console.log(books.publishDate)
-  });
+function compareByPublishedDate(book1, book2) {
+  return book1.publishDate - book2.publishDate;
+}
+
+console.log(books.sort(compareByPublishedDate));
+
 
 //Sort books from oldest to most recent
 
 
-  
+const compareByAlphabetical = books.sort((book1, book2) => {
+  const title1 = book1.name.toUpperCase(); // ignore upper and lowercase
+  const title2 = book2.name.toUpperCase(); // ignore upper and lowercase
+  if (title1 < title2) {
+    return -1;
+  }
+  if (title1 > title2) {
+    return 1;
+  }
+  // names must be equal
+  return 0;
+});
+
+console.log(compareByAlphabetical);
 //sort books alphabetically
 
- 
+
+books.forEach(book => {
+  if (book.name === "War and Peace")
+  console.log(`${book.authorFirst} ${book.authorLast} wrote War and Peace`);
+});
 //Find who wrote War and Peace
+
+before1900 = []
+books.forEach(book => {
+  if (book.publishDate < 1900) {
+    before1900.push(book);
+  }
+});
+console.log(`${before1900.length} books written before 1900`);
 
 //how many books were written before 1900?
 
+
 //was there at least one book published within the last 100 years?
 
+
 //was every book published within the last 100 years?
+
 
 //print a list of books that "includes" the genre historical
 
